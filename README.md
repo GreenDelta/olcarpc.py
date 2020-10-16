90300e6ec100a5af26c306ef89c5232545cd8982
msrocka <michael.srocka@gmail.com>
2020-10-16 15:24:11 +0200

    call it `olcarpc` for now

---
+++ README.md
@@ -0,0 +1,3 @@
+# olca-grpc.py
+This is an experiment to replace the internals of
+[olca-ipc.py](https://github.com/GreenDelta/olca-ipc.py). 
---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+from os import path
+here = path.abspath(path.dirname(__file__))
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='olcarpc',
+    version='0.0.1',
+    description='A Python gRPC client library for openLCA.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/GreenDelta/olca-grpc.py',
+    packages=find_packages(exclude=["tests", "*.tests", "*.tests.*", "tests.*"]),
+    install_requires=[],
+    keywords=['openLCA', 'life cycle assessment', 'LCA'],
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Environment :: Console",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)",
+        "Programming Language :: Python :: 3.6",
+        "Topic :: Utilities",
+    ]
+)
