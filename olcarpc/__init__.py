b761ca9f49f2093b0200de8b66e6aa5e524b1e03
msrocka <michael.srocka@gmail.com>
2020-10-29 14:33:45 +0100

    test structure

--- olcarpc/__init__.py
+++ olcarpc/__init__.py
@@ -89,27 +89,22 @@ class Client:
                 r.type = ref.type
         return self.data.delete(r)
 
-    @property
     def actors(self) -> Iterable[Actor]:
         for actor in self.data.actors(Empty()):
             yield actor
 
-    @property
     def categories(self) -> Iterable[Category]:
         for category in self.data.categories(Empty()):
             yield category
 
-    @property
     def currencies(self) -> Iterable[Currency]:
         for currency in self.data.currencies(Empty()):
             yield currency
 
-    @property
     def dq_systems(self) -> Iterable[DqSystem]:
         for dqSystem in self.data.dq_systems(Empty()):
             yield dqSystem
 
-    @property
     def flows(self) -> Iterable[Flow]:
         for flow in self.data.flows(Empty()):
             yield flow
@@ -120,57 +115,46 @@ class Client:
     def put_flow(self, flow: Flow) -> RefStatus:
         return self.data.put_flow(flow)
 
-    @property
     def flow_properties(self) -> Iterable[FlowProperty]:
         for p in self.data.flow_properties(Empty()):
             yield p
 
-    @property
     def impact_categories(self) -> Iterable[ImpactCategory]:
         for impact in self.data.impact_categories(Empty()):
             yield impact
 
-    @property
     def impact_methods(self) -> Iterable[ImpactMethod]:
         for method in self.data.impact_methods(Empty()):
             yield method
 
-    @property
     def locations(self) -> Iterable[Location]:
         for location in self.data.locations(Empty()):
             yield location
 
-    @property
     def parameters(self) -> Iterable[Parameter]:
         for parameter in self.data.parameters(Empty()):
             yield parameter
 
-    @property
     def processes(self) -> Iterable[Process]:
         for process in self.data.processes(Empty()):
             yield process
 
-    @property
     def product_systems(self) -> Iterable[ProductSystem]:
         for system in self.data.product_systems(Empty()):
             yield system
 
-    @property
     def projects(self) -> Iterable[Project]:
         for project in self.data.projects(Empty()):
             yield project
 
-    @property
     def social_indicators(self) -> Iterable[SocialIndicator]:
         for indicator in self.data.social_indicators(Empty()):
             yield indicator
 
-    @property
     def sources(self) -> Iterable[Source]:
         for source in self.data.sources(Empty()):
             yield source
 
-    @property
     def unit_groups(self) -> Iterable[UnitGroup]:
         for group in self.data.unit_groups(Empty()):
             yield group
--- tests/test_flows.py
+++ tests/test_flows.py
@@ -7,16 +7,11 @@ class FlowTest(unittest.TestCase):
 
     def setUp(self):
         self.client = rpc.Client()
-        self.flow = rpc.Flow(
-            id=str(uuid.uuid4()),
-            name='Test flow',
-            flow_type=rpc.FlowType.ELEMENTARY_FLOW)
-        self.client.put_flow(self.flow)
 
     def tearDown(self):
         self.client.close()
 
-    def test_not_exists(self):
+    def test_non_existing(self):
         # check for ID
         status = self.client.flow('non existing flow')
         self.assertFalse(status.ok)
@@ -28,12 +23,53 @@ class FlowTest(unittest.TestCase):
         self.assertTrue(len(status.error) > 0)
 
     def test_get_flow(self):
+        flow = self.__flow__()
+
         # check for ID
-        status = self.client.flow(self.flow.id)
+        status = self.client.flow(flow.id)
         self.assertTrue(status.ok)
-        self.assertEquals(status.flow.id, self.flow.id)
+        self.assertEquals(status.flow.id, flow.id)
 
         # check for name
-        status = self.client.flow(name=self.flow.name)
-        self.assertTrue(status)
-        self.assertEquals(status.flow.name, self.flow.name)
+        status = self.client.flow(name=flow.name)
+        self.assertTrue(status.ok)
+        self.assertEquals(status.flow.name, flow.name)
+
+        self.assertTrue(self.client.delete(flow).ok)
+
+    def test_get_flows(self):
+        flows = []
+        for _i in range(0, 10):
+            flows.append(self.__flow__())
+        flow_ids = set()
+        for f in self.client.flows():
+            flow_ids.add(f.id)
+        for f in flows:
+            self.assertTrue(f.id in flow_ids)
+            self.assertTrue(self.client.delete(f).ok)
+
+    def test_flow_atts(self):
+        orig = self.__flow__()
+        clone: rpc.Flow = self.client.flow(orig.id).flow
+
+        self.assertEqual('Flow', clone.type)
+        self.assertEqual(orig.id, clone.id)
+        self.assertEqual(orig.name, clone.name)
+        self.assertEqual(orig.version, clone.version)
+        self.assertEqual(orig.last_change, clone.last_change)
+
+        self.assertEqual(orig.flow_type, clone.flow_type)
+        # TODO: check specific fields
+
+        self.assertTrue(self.client.delete(clone).ok)
+
+    def __flow__(self) -> rpc.Flow:
+        flow = rpc.Flow(
+            id=str(uuid.uuid4()),
+            name='Test flow',
+            version='10.00.000',
+            flow_type=rpc.FlowType.ELEMENTARY_FLOW,
+        )
+        status = self.client.put_flow(flow)
+        self.assertTrue(status.ok)
+        return flow
