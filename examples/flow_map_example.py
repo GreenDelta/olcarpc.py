7d90c3e6df41526fb6694c97ed6b5c9ef63c5c33
msrocka <michael.srocka@gmail.com>
2020-11-27 13:35:19 +0100

    uff, `from` is a keyword of course
    and there is where the Python API gets ugly. so we could rename it to
    `source` and `target` like in the openLCA API. but it should be `from` and `to` in
    the JSON serialization. so maybe we introduce a `proto` field in olca-schema to
    support field renaming? `from` and `to` are not so good names... we should rename
    it later...

--- examples/flow_map_example.py
+++ examples/flow_map_example.py
@@ -26,7 +26,34 @@ def delete():
             raise RuntimeError(status.error)
 
 
+def create():
+    flow_map = rpc.FlowMap(name='hsc2ei.csv')
+    entry = rpc.FlowMapEntry(
+        conversion_factor=1.0,
+        to=rpc.FlowMapRef(
+            flow=rpc.FlowRef(
+                id='64cea105-f89a-4f95-ae44-84ff904a28fc',
+                name='limestone, crushed, washed',
+                ref_unit='kg',
+            ),
+            provider=rpc.ProcessRef(
+                id='a321ecf5-907e-3d93-bcd9-2f1a9b5a1189',
+                name='limestone production, crushed, washed ...',
+                location='RoW'
+            )))
+
+    source: rpc.FlowMapRef = getattr(entry, 'from')
+    source.flow.id = 'hsc/Limestone'
+    source.flow.name = 'Limestone'
+    source.flow.ref_unit = 'kg'
+    flow_map.mappings.append(entry)
+
+    with rpc.Client() as client:
+        client.put_flow_map(flow_map)
+
+
 if __name__ == '__main__':
     # print_flow_maps()
     # copy()
-    delete()
+    # delete()
+    create()
