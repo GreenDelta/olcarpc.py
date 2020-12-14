3b9cf57542af479adc61d78025a0bf99eba06a92
msrocka <michael.srocka@gmail.com>
2020-12-14 20:02:39 +0100

    let defs = [   ['Actor', 'actor', 'actors'],   ['Category', 'category', 'categories'],   ['Currency', 'currency', 'currencies'],   ['DQSystem', 'dq_system', 'dq_systems'],   ['Flow', 'flow', 'flows'],   ['FlowProperty', 'flow_property', 'flow_properties'],   ['ImpactCategory', 'impact_category', 'impact_categories'],   ['ImpactMethod', 'impact_method', 'impact_methods'],   ['Location', 'location', 'locations'],   ['Parameter', 'parameter', 'parameters'],   ['Process', 'process', 'processes'],   ['ProductSystem', 'product_system', 'product_systems'],   ['Project', 'project', 'projects'],   ['SocialIndicator', 'social_indicator', 'social_indicators'],   ['Source', 'source', 'sources'],   ['UnitGroup', 'unit_group', 'unit_groups'], ]
    let plural = name => {
      if (name.endsWith('y')) {
        return name.substring(0, name.length - 1) + 'ies'
      }
      if (name.endsWith('s')) {
        return name + 'es'
      }
      return name + 's'
    }
    
    for (let [type, varr, varrs] of defs) {
      let text = `
        def get_${varrs}(self) -> Iterator[${type}]:
            """
            Get all \`${type}\` instances from the database.
            """
            for ${varr} in self.__data.Get${plural(type)}(Empty()):
              yield ${varr}
    
        def get_${varr}(self, ref_id='', name='') -> ${type}Status:
            """
            Get an instance of \`${type}\` by ID or name from the database.
            """
            return self.__data.Get${type}(Ref(id=ref_id, name=name))
    
        def put_${varr}(self, ${varr}: ${type}) -> RefStatus:
            """
            Save or update the given \`${type}\` instance in the database.
            """
            return self.__data.Put${type}(${varr})`
      console.log(text)
    }
    ```

--- olcarpc/__init__.py
+++ olcarpc/__init__.py
@@ -158,164 +158,299 @@ class Client:
 
     def get_actor(self, ref_id='', name='') -> ActorStatus:
         """
-        Get an `Actor` by ID or name from the database.
+        Get an instance of `Actor` by ID or name from the database.
         """
         return self.__data.GetActor(Ref(id=ref_id, name=name))
 
     def put_actor(self, actor: Actor) -> RefStatus:
         """
-        Save or update an `Actor` in the database.
+        Save or update the given `Actor` instance in the database.
         """
         return self.__data.PutActor(actor)
 
-    def categories(self) -> Iterable[Category]:
+    def get_categories(self) -> Iterator[Category]:
+        """
+        Get all `Category` instances from the database.
+        """
         for category in self.__data.GetCategories(Empty()):
             yield category
 
-    def category(self, id='', name='') -> CategoryStatus:
-        return self.__data.GetCategory(Ref(id=id, name=name))
+    def get_category(self, ref_id='', name='') -> CategoryStatus:
+        """
+        Get an instance of `Category` by ID or name from the database.
+        """
+        return self.__data.GetCategory(Ref(id=ref_id, name=name))
 
     def put_category(self, category: Category) -> RefStatus:
+        """
+        Save or update the given `Category` instance in the database.
+        """
         return self.__data.PutCategory(category)
 
-    def currencies(self) -> Iterable[Currency]:
+    def get_currencies(self) -> Iterator[Currency]:
+        """
+        Get all `Currency` instances from the database.
+        """
         for currency in self.__data.GetCurrencies(Empty()):
             yield currency
 
-    def currency(self, id='', name='') -> CurrencyStatus:
-        return self.__data.GetCurrency(Ref(id=id, name=name))
+    def get_currency(self, ref_id='', name='') -> CurrencyStatus:
+        """
+        Get an instance of `Currency` by ID or name from the database.
+        """
+        return self.__data.GetCurrency(Ref(id=ref_id, name=name))
 
     def put_currency(self, currency: Currency) -> RefStatus:
+        """
+        Save or update the given `Currency` instance in the database.
+        """
         return self.__data.PutCurrency(currency)
 
-    def dq_systems(self) -> Iterable[DQSystem]:
-        for dqSystem in self.__data.GetDQSystems(Empty()):
-            yield dqSystem
+    def get_dq_systems(self) -> Iterator[DQSystem]:
+        """
+        Get all `DQSystem` instances from the database.
+        """
+        for dq_system in self.__data.GetDQSystems(Empty()):
+            yield dq_system
 
-    def dq_system(self, id='', name='') -> DQSystemStatus:
-        return self.__data.GetDQSystem(Ref(id=id, name=name))
+    def get_dq_system(self, ref_id='', name='') -> DQSystemStatus:
+        """
+        Get an instance of `DQSystem` by ID or name from the database.
+        """
+        return self.__data.GetDQSystem(Ref(id=ref_id, name=name))
 
     def put_dq_system(self, dq_system: DQSystem) -> RefStatus:
+        """
+        Save or update the given `DQSystem` instance in the database.
+        """
         return self.__data.PutDQSystem(dq_system)
 
-    def flows(self) -> Iterable[Flow]:
+    def get_flows(self) -> Iterator[Flow]:
+        """
+        Get all `Flow` instances from the database.
+        """
         for flow in self.__data.GetFlows(Empty()):
             yield flow
 
-    def flow(self, id='', name='') -> FlowStatus:
-        return self.__data.GetFlow(Ref(id=id, name=name))
+    def get_flow(self, ref_id='', name='') -> FlowStatus:
+        """
+        Get an instance of `Flow` by ID or name from the database.
+        """
+        return self.__data.GetFlow(Ref(id=ref_id, name=name))
 
     def put_flow(self, flow: Flow) -> RefStatus:
+        """
+        Save or update the given `Flow` instance in the database.
+        """
         return self.__data.PutFlow(flow)
 
-    def flow_properties(self) -> Iterable[FlowProperty]:
-        for p in self.__data.GetFlowProperties(Empty()):
-            yield p
+    def get_flow_properties(self) -> Iterator[FlowProperty]:
+        """
+        Get all `FlowProperty` instances from the database.
+        """
+        for flow_property in self.__data.GetFlowProperties(Empty()):
+            yield flow_property
 
-    def flow_property(self, id='', name='') -> FlowPropertyStatus:
-        return self.__data.GetFlowProperty(Ref(id=id, name=name))
+    def get_flow_property(self, ref_id='', name='') -> FlowPropertyStatus:
+        """
+        Get an instance of `FlowProperty` by ID or name from the database.
+        """
+        return self.__data.GetFlowProperty(Ref(id=ref_id, name=name))
 
     def put_flow_property(self, flow_property: FlowProperty) -> RefStatus:
+        """
+        Save or update the given `FlowProperty` instance in the database.
+        """
         return self.__data.PutFlowProperty(flow_property)
 
-    def impact_categories(self) -> Iterable[ImpactCategory]:
-        for impact in self.__data.GetImpactCategories(Empty()):
-            yield impact
+    def get_impact_categories(self) -> Iterator[ImpactCategory]:
+        """
+        Get all `ImpactCategory` instances from the database.
+        """
+        for impact_category in self.__data.GetImpactCategories(Empty()):
+            yield impact_category
 
-    def impact_category(self, id='', name='') -> ImpactCategoryStatus:
-        return self.__data.GetImpactCategory(Ref(id=id, name=name))
+    def get_impact_category(self, ref_id='', name='') -> ImpactCategoryStatus:
+        """
+        Get an instance of `ImpactCategory` by ID or name from the database.
+        """
+        return self.__data.GetImpactCategory(Ref(id=ref_id, name=name))
 
     def put_impact_category(self, impact_category: ImpactCategory) -> RefStatus:
+        """
+        Save or update the given `ImpactCategory` instance in the database.
+        """
         return self.__data.PutImpactCategory(impact_category)
 
-    def impact_methods(self) -> Iterable[ImpactMethod]:
-        for method in self.__data.GetImpactMethods(Empty()):
-            yield method
+    def get_impact_methods(self) -> Iterator[ImpactMethod]:
+        """
+        Get all `ImpactMethod` instances from the database.
+        """
+        for impact_method in self.__data.GetImpactMethods(Empty()):
+            yield impact_method
 
-    def impact_method(self, id='', name='') -> ImpactMethodStatus:
-        return self.__data.GetImpactMethod(Ref(id=id, name=name))
+    def get_impact_method(self, ref_id='', name='') -> ImpactMethodStatus:
+        """
+        Get an instance of `ImpactMethod` by ID or name from the database.
+        """
+        return self.__data.GetImpactMethod(Ref(id=ref_id, name=name))
 
     def put_impact_method(self, impact_method: ImpactMethod) -> RefStatus:
+        """
+        Save or update the given `ImpactMethod` instance in the database.
+        """
         return self.__data.PutImpactMethod(impact_method)
 
-    def locations(self) -> Iterable[Location]:
+    def get_locations(self) -> Iterator[Location]:
+        """
+        Get all `Location` instances from the database.
+        """
         for location in self.__data.GetLocations(Empty()):
             yield location
 
-    def location(self, id='', name='') -> LocationStatus:
-        return self.__data.GetLocation(Ref(id=id, name=name))
+    def get_location(self, ref_id='', name='') -> LocationStatus:
+        """
+        Get an instance of `Location` by ID or name from the database.
+        """
+        return self.__data.GetLocation(Ref(id=ref_id, name=name))
 
     def put_location(self, location: Location) -> RefStatus:
+        """
+        Save or update the given `Location` instance in the database.
+        """
         return self.__data.PutLocation(location)
 
-    def parameters(self) -> Iterable[Parameter]:
+    def get_parameters(self) -> Iterator[Parameter]:
+        """
+        Get all `Parameter` instances from the database.
+        """
         for parameter in self.__data.GetParameters(Empty()):
             yield parameter
 
-    def parameter(self, id='', name='') -> ParameterStatus:
-        return self.__data.GetParameter(Ref(id=id, name=name))
+    def get_parameter(self, ref_id='', name='') -> ParameterStatus:
+        """
+        Get an instance of `Parameter` by ID or name from the database.
+        """
+        return self.__data.GetParameter(Ref(id=ref_id, name=name))
 
     def put_parameter(self, parameter: Parameter) -> RefStatus:
+        """
+        Save or update the given `Parameter` instance in the database.
+        """
         return self.__data.PutParameter(parameter)
 
-    def processes(self) -> Iterable[Process]:
+    def get_processes(self) -> Iterator[Process]:
+        """
+        Get all `Process` instances from the database.
+        """
         for process in self.__data.GetProcesses(Empty()):
             yield process
 
-    def process(self, id='', name='') -> ProcessStatus:
-        return self.__data.GetProcess(Ref(id=id, name=name))
+    def get_process(self, ref_id='', name='') -> ProcessStatus:
+        """
+        Get an instance of `Process` by ID or name from the database.
+        """
+        return self.__data.GetProcess(Ref(id=ref_id, name=name))
 
     def put_process(self, process: Process) -> RefStatus:
+        """
+        Save or update the given `Process` instance in the database.
+        """
         return self.__data.PutProcess(process)
 
-    def product_systems(self) -> Iterable[ProductSystem]:
-        for system in self.__data.GetProductSystems(Empty()):
-            yield system
+    def get_product_systems(self) -> Iterator[ProductSystem]:
+        """
+        Get all `ProductSystem` instances from the database.
+        """
+        for product_system in self.__data.GetProductSystems(Empty()):
+            yield product_system
 
-    def product_system(self, id='', name='') -> ProductSystemStatus:
-        return self.__data.GetProductSystem(Ref(id=id, name=name))
+    def get_product_system(self, ref_id='', name='') -> ProductSystemStatus:
+        """
+        Get an instance of `ProductSystem` by ID or name from the database.
+        """
+        return self.__data.GetProductSystem(Ref(id=ref_id, name=name))
 
     def put_product_system(self, product_system: ProductSystem) -> RefStatus:
+        """
+        Save or update the given `ProductSystem` instance in the database.
+        """
         return self.__data.PutProductSystem(product_system)
 
-    def projects(self) -> Iterable[Project]:
+    def get_projects(self) -> Iterator[Project]:
+        """
+        Get all `Project` instances from the database.
+        """
         for project in self.__data.GetProjects(Empty()):
             yield project
 
-    def project(self, id='', name='') -> ProjectStatus:
-        return self.__data.GetProject(Ref(id=id, name=name))
+    def get_project(self, ref_id='', name='') -> ProjectStatus:
+        """
+        Get an instance of `Project` by ID or name from the database.
+        """
+        return self.__data.GetProject(Ref(id=ref_id, name=name))
 
     def put_project(self, project: Project) -> RefStatus:
+        """
+        Save or update the given `Project` instance in the database.
+        """
         return self.__data.PutProject(project)
 
-    def social_indicators(self) -> Iterable[SocialIndicator]:
-        for indicator in self.__data.GetSocialIndicators(Empty()):
-            yield indicator
+    def get_social_indicators(self) -> Iterator[SocialIndicator]:
+        """
+        Get all `SocialIndicator` instances from the database.
+        """
+        for social_indicator in self.__data.GetSocialIndicators(Empty()):
+            yield social_indicator
 
-    def social_indicator(self, id='', name='') -> SocialIndicatorStatus:
-        return self.__data.GetSocialIndicator(Ref(id=id, name=name))
+    def get_social_indicator(self, ref_id='', name='') -> SocialIndicatorStatus:
+        """
+        Get an instance of `SocialIndicator` by ID or name from the database.
+        """
+        return self.__data.GetSocialIndicator(Ref(id=ref_id, name=name))
 
     def put_social_indicator(self, social_indicator: SocialIndicator) -> RefStatus:
+        """
+        Save or update the given `SocialIndicator` instance in the database.
+        """
         return self.__data.PutSocialIndicator(social_indicator)
 
-    def sources(self) -> Iterable[Source]:
+    def get_sources(self) -> Iterator[Source]:
+        """
+        Get all `Source` instances from the database.
+        """
         for source in self.__data.GetSources(Empty()):
             yield source
 
-    def source(self, id='', name='') -> SourceStatus:
-        return self.__data.GetSource(Ref(id=id, name=name))
+    def get_source(self, ref_id='', name='') -> SourceStatus:
+        """
+        Get an instance of `Source` by ID or name from the database.
+        """
+        return self.__data.GetSource(Ref(id=ref_id, name=name))
 
     def put_source(self, source: Source) -> RefStatus:
+        """
+        Save or update the given `Source` instance in the database.
+        """
         return self.__data.PutSource(source)
 
-    def unit_groups(self) -> Iterable[UnitGroup]:
-        for group in self.__data.GetUnitGroups(Empty()):
-            yield group
+    def get_unit_groups(self) -> Iterator[UnitGroup]:
+        """
+        Get all `UnitGroup` instances from the database.
+        """
+        for unit_group in self.__data.GetUnitGroups(Empty()):
+            yield unit_group
 
-    def unit_group(self, id='', name='') -> UnitGroupStatus:
-        return self.__data.GetUnitGroup(Ref(id=id, name=name))
+    def get_unit_group(self, ref_id='', name='') -> UnitGroupStatus:
+        """
+        Get an instance of `UnitGroup` by ID or name from the database.
+        """
+        return self.__data.GetUnitGroup(Ref(id=ref_id, name=name))
 
     def put_unit_group(self, unit_group: UnitGroup) -> RefStatus:
+        """
+        Save or update the given `UnitGroup` instance in the database.
+        """
         return self.__data.PutUnitGroup(unit_group)
 
     def get_providers_of(
