691a11b90f26300ba29270c672ec3b6b9ab8a10a
msrocka <michael.srocka@gmail.com>
2021-11-26 13:37:17 +0100

    a single call

--- generate.py
+++ generate.py
@@ -1,4 +1,3 @@
-import os
 import subprocess
 
 
@@ -6,23 +5,13 @@ import subprocess
 #  pip install mypy-protobuf
 
 def main():
-    for proto in os.listdir('proto'):
-        print(proto)
-        subprocess.call(
-            'py -m grpc_tools.protoc'
-            ' -I./proto'
-            ' --python_out=./olcarpc/generated'
-            ' --grpc_python_out=./olcarpc/generated'
-            ' --mypy_out=./olcarpc/generated'
-            ' ./proto/' + proto)
-        """
-        subprocess.call(
-            'protoc'
-            ' -I./proto'
-            ' --python_out=./olcarpc/generated'
-            ' --mypy_out=./olcarpc/generated'
-            ' ./proto/' + proto)
-            """
+    subprocess.call(
+        'py -m grpc_tools.protoc'
+        ' -I./proto'
+        ' --python_out=./olcarpc/generated'
+        ' --grpc_python_out=./olcarpc/generated'
+        ' --mypy_out=./olcarpc/generated'
+        ' ./proto/*.proto')
 
 
 if __name__ == '__main__':
