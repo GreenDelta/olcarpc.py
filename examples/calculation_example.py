e0fcfd03963a620463c3edb7a689bf41ddcdc4a8
msrocka <michael.srocka@gmail.com>
2022-08-22 16:47:14 +0200

    fixed flow example

--- examples/calculation_example.py
+++ examples/calculation_example.py
@@ -1,18 +1,22 @@
 import olcarpc as rpc
 
 if __name__ == '__main__':
+
     with rpc.Client() as client:
 
-        # create a calculation setup
+        # Create a calculation setup with the ID of a product system
+        # and LCIA method. The result is then related to the quantitative
+        # reference amount of that system, if not specified otherwise
         system = rpc.ProtoRef(id='7d1cbce0-b5b3-47ba-95b5-014ab3c7f569')
         method = rpc.ProtoRef(id='99b9d86b-ec6f-4610-ba9f-68ebfe5691dd')
         setup = rpc.ProtoCalculationSetup(
             product_system=system,
             impact_method=method)
 
+        # run the calculation
         result = client.results.Calculate(setup)
 
-        # query the inventory
+        # print the first 10 items of the inventory result
         i = 0
         for r in client.results.GetTotalInventory(result):
             print(r)
@@ -20,8 +24,10 @@ if __name__ == '__main__':
             if i == 10:
                 break
 
-        # query the impact results
+        # print the LCIA result
         for r in client.results.GetTotalImpacts(result):
             print(r)
 
+        # dispose the result after a calculation if it is not needed anymore,
+        # otherwise it will stay in memory
         client.results.Dispose(result)
--- examples/flow_example.py
+++ examples/flow_example.py
@@ -1,12 +1,35 @@
 import olcarpc as rpc
+import uuid
 
 
 def main():
     with rpc.Client() as client:
-        mass = client.get_flow_property(name='Mass').flow_property
-        flow = rpc.product_flow_of('butter, fried', mass)
-        client.put_flow(flow)
-        print(client.get_flow(flow.id))
+        # get the flow property 'Mass' by name
+        dataset: rpc.ProtoDataSet = client.fetch.Find(
+            rpc.FindRequest(type=rpc.FlowProperty, name='Mass'))
+
+        mass: rpc.ProtoFlowProperty = dataset.flow_property
+        if mass is None:
+            print('could not find flow property Mass')
+            return
+
+        # create a product flow with mass as reference flow property
+        flow = rpc.ProtoFlow(
+            id=str(uuid.uuid4()),
+            name="my product flow",
+            flow_type=rpc.ProtoFlowType.PRODUCT_FLOW,
+            flow_properties=[
+                rpc.ProtoFlowPropertyFactor(
+                    flow_property=rpc.ProtoRef(id=mass.id),
+                    is_ref_flow_property=True,
+                    conversion_factor=1.0)
+            ])
+
+        # insert the flow in the database
+        client.update.Put(rpc.ProtoDataSet(flow=flow))
+
+        # get it via ID
+        print(client.fetch.Get(rpc.GetRequest(type=rpc.Flow, id=flow.id)))
 
 
 if __name__ == '__main__':
